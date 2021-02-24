3d79b1710eeabe02795b5de452b2cbf4d68602db
seba <sebastian.davila.personal@gmail.com>
2021-02-24 11:30:14 -0300

    Testing accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,62 +1,57 @@
-from django.contrib.auth import get_user_model
 from django import forms
+from django.contrib.auth import get_user_model
 
 non_allowed_usernames = [
-    'sarasa',
-    'marchanta',
+    "sarasa",
+    "marchanta",
 ]
 User = get_user_model()
 
 
 class RegisterForm(forms.Form):
     username = forms.CharField(max_length=100)
-    email= forms.EmailField()
+    email = forms.EmailField()
     password1 = forms.CharField(
         max_length=100,
         label="Password",
-        widget= forms.PasswordInput(
+        widget=forms.PasswordInput(
             attrs={
                 "class": "form-control",
             }
-        ), 
-        
-        )
+        ),
+    )
     password2 = forms.CharField(
         max_length=100,
         label="confirm-password",
-        widget= forms.PasswordInput(
+        widget=forms.PasswordInput(
             attrs={
                 "class": "form-control",
             }
-        ), 
-        
-        )
-    
+        ),
+    )
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(
         max_length=100,
-        widget= forms.PasswordInput(
+        widget=forms.PasswordInput(
+            # Estos son atributos que luego se agregan en la etiqueta html correspondiente
             attrs={
                 "class": "form-control",
             }
-        ), 
-        
-        )
+        ),
+    )
 
     # def clean(self):
     #     username = self.cleaned_data.get("username")
     #     password = self.cleaned_data.get("password")
-#   En este bootcamp vamos a hacer más validaciones en las vistas
-# Pero lo ideal es que las validaciones estén acá.
-
-
-
+    #   En este bootcamp vamos a hacer más validaciones en las vistas
+    # Pero lo ideal es que las validaciones estén acá.
 
     def clean_username(self):
         username = self.cleaned_data.get("username")
-        # UNA VEZ Q LIMPIAMOS LA DATA, CHECKEAMOS QUE NO HAYA OTRO 
+        # UNA VEZ Q LIMPIAMOS LA DATA, CHECKEAMOS QUE NO HAYA OTRO
         # USUARIO CON EL MISMO NOMBRE
         qs = User.objects.filter(username__iexact=username)
         if not qs.exists():
@@ -64,7 +59,7 @@ class LoginForm(forms.Form):
         # para agregar una lista de usuarios no validos
         # antes hay que declarar la lista arriba de todo
         if username in non_allowed_usernames:
-            raise forms.ValidationError("This is an invalid user, please pick another.")   
+            raise forms.ValidationError("This is an invalid user, please pick another.")
         return username
 
     def clean_email(self):
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,69 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+
+User = get_user_model()
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        user_a = User(username="UsuariodePrueba", email="usuario@deprueba.com")
+        user_a.is_staff = True
+        user_a.is_superuser = True
+        self.user_a = user_a
+        user_a_pw = "contrasenia"
+        self.user_a_pw = user_a_pw
+        user_a.set_password(user_a_pw)
+        user_a.save()
+        user_a.set_password
+
+    # Vamos a crear un usuario que luego podemos reutilizar
+
+    def test_user_exist(self):
+        """
+        Vamos a probar que se crea sólo un usuario.
+        """
+        user_count = User.objects.all().count()
+        self.assertEqual(user_count, 1)
+
+    def test_user_password_correct(self):
+        """
+        Vamos a probar que se chequee la contraseña correcta. Tendría que retornar True.
+        """
+        self.assertTrue(self.user_a.check_password(self.user_a_pw))
+
+    def test_user_password_incorrect(self):
+        """
+        Tendría que retornar Falso al incorporar una contraseña incorrecta.
+        """
+        self.assertFalse(self.user_a.check_password("sarasa"))
+
+
+class ViewsTestsCase(TestCase):
+    def setUp(self):
+        user_a = User(username="UsuariodePrueba", email="usuario@deprueba.com")
+        user_a.is_staff = True
+        user_a.is_superuser = True
+        self.user_a = user_a
+        user_a_pw = "contrasenia"
+        self.user_a_pw = user_a_pw
+        user_a.set_password(user_a_pw)
+        user_a.save()
+        user_a.set_password
+
+    def test_login_url_view(self):
+        """
+        Si visitamos el view de login, Tiene que ser igual que el login que tenemos configurado en settings.py.
+        Nos debe responder con el código 200.
+        """
+        url_login = settings.LOGIN_URL
+        # response = self.client.post(url, {}, follow=True)
+        data = {"username": "usuario", "password": self.user_a_pw}
+        response = self.client.post(url_login, data, follow=True)
+        # print(dir(response))
+        status_code = response.status_code
+        redirect_path = response.request.get("PATH_INFO")
+        self.assertEqual(url_login, redirect_path)
+        self.assertEqual(status_code, 200)
