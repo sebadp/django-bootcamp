18ef1a27e8d6be23b08dd89362e56be9e13be2a5
seba <sebastian.davila.personal@gmail.com>
2021-02-24 16:36:41 -0300

    Orders views, forms

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect, render
 # Create your views here.
 from products.models import Product
 
+from orders.forms import OrderForm
+
 from .models import Order
 
 
@@ -14,5 +16,25 @@ def order_checkout_view(request):
         return redirect("/")
     product = qs.first()
     user = request.user
-    Order.objects.create(product=product, user=user)
-    return render(request, "forms.html", {})
+    order_id = request.session.get("order_id")
+    order_obj = None
+    new_creation = False
+    try:
+        order_obj = Order.objects.get(id=order_id)
+    except:
+        order_id = None
+
+    if order_id == None:
+        new_creation = True
+        order_obj = Order.objects.create(product=product, user=user)
+    print(order_obj.id)
+    if order_obj != None and new_creation == False:
+        if order_obj.product.id != product.id:
+            order_obj = Order.objects.create(product=product, user=user)
+    request.session["order_id"] = order_obj.id
+    form = OrderForm(request.POST or None, product=product, instance=order_obj)
+    if form.is_valid():
+        order_obj.shipping_address = form.cleaned_data.get("shipping_address")
+        order_obj.billing_address = form.cleaned_data.get("billing_address")
+        order_obj.save()
+    return render(request, "forms.html", {"form": form})
