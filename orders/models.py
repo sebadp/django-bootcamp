4884baf9e70750d71cd7bff577abfb974d18c5f9
seba <sebastian.davila.personal@gmail.com>
2021-02-26 07:51:05 -0300

    Fixing Orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save, pre_save
@@ -27,16 +29,31 @@ class Order(models.Model):
     shipping_address = models.TextField(blank=True, null=True)
     billing_address = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    inventory_updated = models.BooleanField(default=False)
+
+    def mark_paid(self, custom_amount=None, save=False):
+        paid_amount = self.total
+        if custom_amount != None:
+            paid_amount = custom_amount
+        self.paid = paid_amount
+        self.status = 'paid'
+        if not self.inventory_updated and instance.product:
+            instance.product.remove_item_from_inventory(save=True)
+
+            self.inventory_updated = True
+        if save == True:
+            self.save()
+        retunr self.paid
 
     def calculate(self):
         if not self.product:
             return {}
         subtotal = self.product.price
-        tax_rate = 0.12
+        tax_rate = Decimal(0.12)
         tax_total = tax_rate * subtotal
-        tax_total = float("%.2f" % (tax_total))
-        total = price + tax_total
-        total = float("%2f" % (total))
+        tax_total = Decimal("%.2f" % (tax_total))
+        total = subtotal + tax_total
+        total = Decimal("%2f" % (total))
         totals = {"subtotal": subtotal, "tax": tax_total, "total": total}
 
         for k, v in totals.items():
@@ -44,3 +61,10 @@ class Order(models.Model):
             if save == True:
                 self.save()
         return totals
+
+
+def order_pre_save(sender, instance, *args, **kwargs):
+    instance.calculate(save=False)
+
+
+pre_save.conect(order_pre_save, sender=Order)
